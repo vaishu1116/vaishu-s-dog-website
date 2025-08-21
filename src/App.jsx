c691c9b9a54f27901354f64c6a5d9f2f8dc04aaa
Vaishnavi Gaikwad <vaishnavigaikwad1116@gmail.com>
2025-08-21 19:19:31 +0530

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ const App = () => {
   return(
     <Router>
       <Routes>
+        <Route path="/" element={<Home/>}/>
         <Route path="/home" element={<Home/>}/>
         
         <Route path="/americandog" element={<AmericanDog/>}/>
